ea385d69636db49de8ba0f236f4d25f51c2223b1
Enrique Gamba <we.gamba125@uniandes.edu.co>
2022-09-17 18:00:59 -0500

    Controller ciudad y supermercado

--- src/supermercado/supermercado.controller.ts
+++ src/supermercado/supermercado.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { SupermercadoDto } from 'src/dto/supermercado.dto';
+import { BusinessErrorsInterceptor } from 'src/shared/interceptors/business-errors.interceptor';
+import { SupermercadoEntity } from './supermercado.entity';
+import { SupermercadoService } from './supermercado.service';
 
-@Controller('supermercado')
-export class SupermercadoController {}
+@Controller('supermarkets')
+@UseInterceptors(BusinessErrorsInterceptor)
+export class SupermercadoController {
+    constructor(private readonly supermarketService: SupermercadoService) {}
+
+    @Get()
+    async findAll() {
+      return await this.supermarketService.findAll();
+    }
+  
+    @Get(':supermarketId')
+    async findOne(@Param('supermarketId') supermarketId: string) {
+      return await this.supermarketService.findOne(supermarketId);
+    }
+  
+    @Post()
+    async create(@Body() supermarketDto: SupermercadoDto) {
+      const supermarket: SupermercadoEntity = plainToInstance(SupermercadoEntity, supermarketDto);
+      return await this.supermarketService.create(supermarket);
+    }
+  
+    @Put(':supermarketId')
+    async update(@Param('supermarketId') supermarketId: string, @Body() supermarketDto: SupermercadoDto) {
+      const supermarket: SupermercadoEntity = plainToInstance(SupermercadoEntity, supermarketDto);
+      return await this.supermarketService.update(supermarketId, supermarket);
+    }
+  
+    @Delete(':supermarketId')
+    @HttpCode(204)
+    async delete(@Param('supermarketId') supermarketId: string) {
+      return await this.supermarketService.delete(supermarketId);
+    }
+}
