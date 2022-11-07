285ccd2b24e9d72886762235faa224b7b4f07a06
Enrique Gamba <we.gamba125@uniandes.edu.co>
2022-11-06 22:16:02 -0500

    Update supermercado.controller.ts

--- src/supermercado/supermercado.controller.ts
+++ src/supermercado/supermercado.controller.ts
@@ -23,11 +23,13 @@ export class SupermercadoController {
     @Post()
     async create(@Body() supermarketDto: SupermercadoDto) {
       const supermarket: SupermercadoEntity = plainToInstance(SupermercadoEntity, supermarketDto);
+      const supermarket: SupermercadoEntity = plainToInstance(SupermercadoEntity, supermarketDto);
       return await this.supermarketService.create(supermarket);
     }
   
     @Put(':supermarketId')
     async update(@Param('supermarketId') supermarketId: string, @Body() supermarketDto: SupermercadoDto) {
+      const supermarket: SupermercadoEntity = plainToInstance(SupermercadoEntity, supermarketDto);
       const supermarket: SupermercadoEntity = plainToInstance(SupermercadoEntity, supermarketDto);
       return await this.supermarketService.update(supermarketId, supermarket);
     }
